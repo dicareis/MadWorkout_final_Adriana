082569d2f64b0e920dfe60f78bbf4b7c1e07c2a0
eleves <eleves@mac304-02.local>
2017-08-23 15:52:39 -0400

    Version1

---
+++ MadWorkout WatchKit Extension/WorkoutController.swift
@@ -0,0 +1,26 @@
+//
+//  WorkoutController.swift
+//  MadWorkout
+//
+//  Created by eleves on 2017-08-17.
+//  Copyright © 2017 GrassetSucre. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class WorkoutController: WKInterfaceController {
+    
+    
+    @IBOutlet var displayLabel: WKInterfaceLabel!
+    
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        let temp = context as? [String : String]
+        displayLabel.setText(temp?["workout"])
+    }
+
+}
